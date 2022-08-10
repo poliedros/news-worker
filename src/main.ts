35a5da7e8fbbab6494e20044df006235a7f7a45a
Carlos <carloshlzansavio@gmail.com>
2022-08-10 15:46:31 +0100

    feat: add logging

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { ServiceUnavailableException } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { RedisService } from './redis/redis.service';
 import { NewsapiService } from './newsapi/newsapi.service';
+import { News } from './interfaces';
+
+function printNews(news: News[], n: number) {
+  const { headline, by, publishedAt } = news[n];
+  console.log(`Headline: ${headline}`);
+  console.log(`By: ${by}`);
+  console.log(`Published at: ${publishedAt}`);
+  console.log('\n');
+}
 
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(AppModule);
@@ -14,6 +23,13 @@ async function bootstrap() {
   if (data.status !== 'ok') throw new ServiceUnavailableException();
 
   const res = newsService.transformData(data);
+
+  console.log(`First article saved on Redis:`);
+  printNews(res, 0);
+
+  console.log(`Second article saved on Redis:`);
+  printNews(res, 2);
+
   redisService.save('NEWS_DATA', res);
 
   await app.close();
